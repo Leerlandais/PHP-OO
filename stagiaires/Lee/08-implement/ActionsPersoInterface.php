d904faa5b80ef5f769551225e34d867755c7fd8d
leerlandais <leerlandais@gmail.com>
2024-06-06 12:09:06 +0200

    lunch

---
+++ stagiaires/Lee/08-implement/ActionsPersoInterface.php
@@ -0,0 +1,10 @@
+<?php
+// interface can only have public methods
+interface ActionPersoInterface {
+
+     public function attack($enemy) : string;
+     public function defence() : array;
+
+
+
+}
